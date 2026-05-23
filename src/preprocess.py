8f6b644c18f697feaf83f94e841fdb44696db2e4
Labhansh Goyal <labhanshgoyal465@gmail.com>
2026-05-23 23:12:02 +0530

    added clean data block in the preprocessing file.

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,3 +10,11 @@ def load_data(filepath):
     df = df.iloc[:,2:]
     print(f"Loaded dataset: {df.shape[0]} rows, {df.shape[1]} columns")
     return df
+
+    def clean_data(df):
+    df = df.copy()
+    df['Dependents'] = df['Dependents'].replace('3+', '3')
+    df['Dependents'] = pd.to_numeric(df['Dependents'], errors='coerce')
+    if 'Total_Income' in df.columns:
+        df.drop('Total_Income', axis=1, inplace=True)
+    return df
